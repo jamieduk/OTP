cb6395f23a1e1f3ab4e76ee0a89560b088b3ee6e
Jay <jamied_uk@hotmail.com>
2022-01-30 23:23:10 +0000

    Add files via upload

--- encrypt.sh
+++ encrypt.sh
@@ -7,10 +7,9 @@
 #path="/home/$USER/Documents/Scripts/LanBackup_Full"
 #
 # To Manually Do It
-
-
+input=`cat text.txt`
 # Encrypt the plaintext with the otp
-./otp.sh -e test.txt otp.txt > cipher.txt
+./otp.sh -e "$input" otp.txt > cipher.txt
 
 echo ""
 cat cipher.txt
