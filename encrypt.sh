4cbdc8ac4736af66623d80580dd697632c4f018e
Jay <jamied_uk@hotmail.com>
2022-01-31 00:36:37 +0000

    Add files via upload

--- encrypt.sh
+++ encrypt.sh
@@ -8,12 +8,15 @@
 #
 # To Manually Do It
 input=`cat text.txt`
+echo "Your Text $input To Be Encrypted"
+echo ""
 # Encrypt the plaintext with the otp
 ./otp.sh -e "$input" otp.txt > cipher.txt
-
 echo ""
+echo "All Done!"
+echo "Your Cipher Text Is"
 cat cipher.txt
 echo ""
-echo "Thats Your Cipher Text, Key Is Stored As otp.txt, Ready To Go Back To Menu?"
+echo "Key Is Stored As otp.txt, Ready To Go Back To Menu?"
 read Y
 bash menu.sh
